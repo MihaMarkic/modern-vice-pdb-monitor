2fc75491e337036c3cd9215ca27627be1b6c8bce
Miha Markič <miha@rthand.com>
2023-05-14 15:47:18 +0200

    Renames Modern.Vice.PdbMonitor.Utils.Test to Modern.Vice.PdbMonitor.Engine.Test, creates initial frosting, adds initial squirrel

--- source/BuildTool/BuildTool/build/PublishTask.cs
+++ source/BuildTool/BuildTool/build/PublishTask.cs
@@ -1,10 +1,22 @@
-[TaskName("Publish")]
+[TaskName("SquirrelPublish")]
 [IsDependentOn(typeof(TestTask))]
+public sealed class SquirrelPublishTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext context) => context.ProjectType == ProjectType.Squirrel;
+    public override void Run(BuildContext context)
+    {
+        context.Log.Information("Squirrel");
+    }
+}
+
+
+[TaskName("Publish")]
+[IsDependentOn(typeof(SquirrelPublishTask))]
 public sealed class PublishTask : FrostingTask<BuildContext>
 {
     // Tasks can be asynchronous
     public override void Run(BuildContext context)
     {
-        context.Log.Information("World");
+        context.Log.Information("Publish");
     }
 }
